ef7b97965efdee35e69b09b4c9800c1f0232f1fc
Nick Palenchar <nickpal@nickpalenchar.com>
2020-10-02 13:57:34 -0400

    gitconfig

--- shell-setup.sh
+++ shell-setup.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/env zsh
-set -x
+#!/usr/bin/env bash 
+#set -x
 
 #######
 # zsh
 ######
 
 #chsh -s /bin/zsh
+
+if [[ -z "$(which brew)" ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
 
 ##############
 # PREZTO
@@ -21,7 +25,29 @@ fi
 # how2
 #######
 
-if [[ -z "$(which how2)" && -n "which npm" ]]; then
+if [[ -z "$(which how2)" && -n "$(which npm)" ]]; then
     echo "setting up how2"
     sudo npm install -g how-2
+fi
+
+#########
+# iPython
+# #######
+
+if [[ -z "$(which ipython)" ]]; then
+    echo "setting up ipython"
+    if [[ -n "$(which pip)" ]]; then
+        pip install ipython
+    elif [[ -n "$(which pip3)" ]]; then
+        pip3 install ipython
+    fi
+fi 
+
+#####
+# fff
+# ###
+#
+
+if [[ -z "$(which fff)" ]]; then
+    brew install fff
 fi
