6c29666b6cb030c41a358a702bac790026df7ef6
Nick Palenchar <nickpal@nickpalenchar.com>
2022-05-12 11:27:36 -0400

    histdb for zsh-autosuggestions

--- oh-my-zsh-custom/zsh-autosuggest.config.zsh
+++ oh-my-zsh-custom/zsh-autosuggest.config.zsh
@@ -2,7 +2,34 @@
 ZSH_AUTOSUGGEST_HISTORY_IGNORE="(cd *|ls *)"
 
 
+# histdb_top_here: This query will find the most frequently issued command that is issued in the current directory or any subdirectory. 
+_zsh_autosuggest_strategy_histdb_top_here() {
+    local query="select commands.argv from
+history left join commands on history.command_id = commands.rowid
+left join places on history.place_id = places.rowid
+where places.dir LIKE '$(sql_escape $PWD)%'
+and commands.argv LIKE '$(sql_escape $1)%'
+group by commands.argv order by count(*) desc limit 1"
+    suggestion=$(_histdb_query "$query")
+}
+
+# This will find the most frequently issued command issued exactly in this directory, or if there are no matches it will find the most frequently issued command in any directory.
+_zsh_autosuggest_strategy_histdb_top() {
+    local query="
+        select commands.argv from history
+        left join commands on history.command_id = commands.rowid
+        left join places on history.place_id = places.rowid
+        where commands.argv LIKE '$(sql_escape $1)%'
+        group by commands.argv, places.dir
+        order by places.dir != '$(sql_escape $PWD)', count(*) desc
+        limit 1
+    "
+    suggestion=$(_histdb_query "$query")
+}
+
 ZSH_AUTOSUGGEST_STRATEGY=(
+  histdb_top
+  histdb_top_here
   history
   completion
 )
