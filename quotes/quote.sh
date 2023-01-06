7b52c14ab6b5c18208ca5946cb32253b9c6215a8
Nick Palenchar <nickpal@nickpalenchar.com>
2023-01-06 09:26:28 -0500

    generate random quotes

---
+++ quotes/quote.sh
@@ -0,0 +1,37 @@
+
+
+quotes() {
+  local file=$1
+  local lines="$(( $(cat $file | wc -l) ))"
+  local pick=$(( ( $RANDOM % ( $lines - 1 ) ) + 1 ))
+  local line1="$(sed -n "$pick,${pick}p" $file)"
+  (( pick++ ))
+  local line2="$(sed -n "$pick,${pick}p" $file)"
+  (( pick++ ))
+  local line3="$(sed -n "$pick,${pick}p" $file)"
+  (( pick++ ))
+  local line4="$(sed -n "$pick,${pick}p" $file)"
+  
+  local quote=""
+  local author=""
+  if [[ "$line1" ]] && [[ "$line2" ]]; then
+    quote=$line1
+    author=$line2
+  elif [[ "$line2" ]] && [[ "$line3" ]]; then
+    quote=$line2
+    author=$line3
+  elif [[ "$line3" ]] && [[ "$line4" ]]; then
+    quote=$line3
+    author=$line4
+  fi
+
+  quote="$(echo $quote | fold -w 40 -s)"
+
+  printf "\e[3m\e[90m${quote}\e[0m\n"
+
+  printf  "  \e[1m\e[90m --$author\e[0m\n"
+
+}
+
+quotes $@
+
