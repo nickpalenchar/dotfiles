ecc491af42fc7f0fb20bee88830791c88c6d9e24
Nick Palenchar <nickpal@nickpalenchar.com>
2024-06-30 09:26:18 -0400

    shorten nvtree root file directory

--- nvim/lua/core/plugin_config/nvimtree_config.lua
+++ nvim/lua/core/plugin_config/nvimtree_config.lua
@@ -1,13 +1,22 @@
+local function label(path)
+  path = path:gsub(os.getenv 'HOME', '~', 1)
+  return path:gsub('([a-zA-Z])[a-z0-9]+', '%1') .. 
+    (path:match '[a-zA-Z]([a-z0-9]*)$' or '')
+end
+local api = require 'nvim-tree.api'
+local nt = require 'nvim-tree'
+nt.setup { renderer = { root_folder_label = label, group_empty = label } }
+
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     width = 30,
   },
-  renderer = {
-    group_empty = true,
+  renderer = { 
+    root_folder_label = label, 
+    group_empty = label 
   },
   filters = {
-    dotfiles = true,
   },
 })
 
