02dc314422b9562ea95c86cc94f1273d14f51145
Nick Palenchar <nickpal@nickpalenchar.com>
2025-03-19 12:55:29 -0400

    cleanup

--- aliases/git.sh
+++ aliases/git.sh
@@ -9,6 +9,7 @@ alias g..='git checkout -'
 alias gd='git diff'
 alias gds='git diff --staged'
 alias gl='git pull'
+alias gca='git commit --amend'
 
 function gpr () {
   local current_branch="$(git rev-parse --abbrev-ref HEAD)"
--- zsh-scripts/kubernetes.sh
+++
@@ -1,5 +0,0 @@
-kns() { kubectl config set-context --current --namespace=$1; }
-kdp() { kubectl describe po $1; }
-alias kjobs="k get jobs"
-alias ksvc="k get svc"
-
