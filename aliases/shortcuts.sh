109fde50a218f76685d939dd524799e0b3ed0cbd
Nick Palenchar <nickpal@nickpalenchar.com>
2025-07-25 09:13:23 -0400

    nuke

--- aliases/shortcuts.sh
+++
@@ -1,6 +0,0 @@
-# more aliases for shell commands, but unlike shell.sh,
-# more specific to personal directories
-
-alias cdm='cd ~/mason'
-alias cdp='cd ~/mason/programming'
-alias cdd='cd ~/dotfiles'
--- conf/zsh/hooks/preexec_log-command.sh
+++
@@ -1,5 +0,0 @@
-preexec() {
-  if [ "$(id -u)" -ne 0 ] && [ -z "$NOLOG" ] && [[ ! "$3" =~ "~/.logs" ]] && [[ ! "$(pwd)" =~ "$HOME/.logs" ]]; then
-    echo "$(date "+%Y-%m-%d.%H:%M") $(pwd) $ $3" >> ~/.logs/zsh-history-$(date "+%Y-%m-%d").log
-  fi
-}
